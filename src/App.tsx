756a4e07fdd75c2ea972f0df290ee8aea85607c6
josefilho <iamjose.filho@gmail.com>
2021-03-22 00:41:10 -0400

    feat[navigation]: created basic navigation with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return <h1>Welcome to Template Typescritp</h1>;
-}
+import Routes from './routes';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
